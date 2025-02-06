4eec92fde2cac12b81a828e036379e0f2bbea039
byeongmin-kwak <bqudmals@naver.com>
2025-02-07 06:10:27 +0900

    feat: 헤더 컴포넌트 추가 및 App에 통합

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Landing from "./pages/landing";
 import Dashboard from "./pages/dashboard";
+import Header from "./shared/header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         {/* 로그인 전 페이지 */}
         <Route exact path="/" element={<Landing />} />

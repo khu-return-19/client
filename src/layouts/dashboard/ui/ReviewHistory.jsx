10342b0eedfda558ff946141ed88ec12a58cc28f
byeongmin-kwak <bqudmals@naver.com>
2025-02-07 07:09:30 +0900

    feat: 대시보드 레이아웃 및 컴포넌트 추가 (MyInfo, ReviewHistory)

---
+++ src/layouts/dashboard/ui/ReviewHistory.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./ReviewHistory.module.scss";
+
+function ReviewHistory() {
+  return <div className={styles.reviewHistory}>ReviewHistory</div>;
+}
+
+export default ReviewHistory;
--- src/pages/dashboard/ui/Dashboard.jsx
+++ src/pages/dashboard/ui/Dashboard.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { MyInfo, ReviewHistory } from "layouts/dashboard";
+import styles from "./Dashboard.module.scss";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div className={styles.dashboard}>
+      <MyInfo />
+      <ReviewHistory />
+    </div>
+  );
 }
 
 export default Dashboard;

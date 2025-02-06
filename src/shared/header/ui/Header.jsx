369265c8f3e546cc0b66e2e1e157a397ca601958
byeongmin-kwak <bqudmals@naver.com>
2025-02-07 06:37:12 +0900

    feat: 헤더 스타일링 및 레이아웃 추가

--- src/shared/header/ui/Header.jsx
+++ src/shared/header/ui/Header.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styles from "./Header.module.scss";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className={styles.header}>
+      <div className={styles.inner}>
+        <div className={styles.logo}>Logo</div>
+        <div className={styles.menu}>login</div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;

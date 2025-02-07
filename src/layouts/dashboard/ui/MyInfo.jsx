2df154dea63f84871937e01caf838d5df4cfcab7
byeongmin-kwak <bqudmals@naver.com>
2025-02-08 06:00:31 +0900

    feat: MyInfo 컴포넌트의 경로 수정

--- src/layouts/dashboard/ui/MyInfo.jsx
+++ src/layouts/dashboard/ui/MyInfo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./MyInfo.module.scss";
-import { MyInfoTitle, Name, CoverLetter } from "components/dashboard";
+import { MyInfoTitle, Name, CoverLetter } from "components/dashboard/myInfo";
 
 function MyInfo() {
   return (

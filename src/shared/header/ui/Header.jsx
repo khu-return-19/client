4e4b61593ec6090b61948f4d0dc2db113678d0d4
byeongmin-kwak <bqudmals@naver.com>
2025-02-12 18:43:15 +0900

    test: 로그인 테스트

--- src/shared/header/ui/Header.jsx
+++ src/shared/header/ui/Header.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Header.module.scss";
+import axios from "axios";
 
 function Header() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const userName = "홍길동";
 
   const handleAuth = () => {
     if (isLoggedIn) {
@@ -12,15 +14,53 @@ function Header() {
     }
   };
 
+  useEffect(() => {
+    axios
+      .get("https://zackinthebox.shop/give")
+      .then((response) => {
+        if (response.status === 200) {
+          setIsLoggedIn(true);
+          console.log(response.data);
+        }
+      })
+      .catch((error) => {
+        setIsLoggedIn(false);
+      });
+  }, []);
+
   return (
     <div className={styles.header}>
-      <div className={styles.inner}>
-        <a href="/" className={styles.logoLink}>
+      <div className={styles.container}>
+        <a href="/" className={styles.logo}>
           <img className={styles.logo} src="/logo.png" alt="로고" />
         </a>
-        <button className={styles.loginButton} onClick={handleAuth}>
-          {isLoggedIn ? "로그아웃" : "로그인"}
-        </button>
+        <div className={styles.menuSection}>
+          <a href="/" className={styles.menu}>
+            서비스 소개
+          </a>
+          <a href="/" className={styles.menu}>
+            공지사항
+          </a>
+          <a href="/profile" className={styles.menu}>
+            내정보
+          </a>
+          <a href="/" className={styles.menu}>
+            자기소개서 분석
+          </a>
+          {isLoggedIn ? (
+            <div className={styles.authSection}>
+              <span className={styles.userName}>{userName}님</span>
+              <div>|</div>
+              <a href="" className={styles.logout} onClick={handleAuth}>
+                로그아웃
+              </a>
+            </div>
+          ) : (
+            <a href="https://zackinthebox.shop/oauth2/authorization/google" className={styles.login}>
+              로그인
+            </a>
+          )}
+        </div>
       </div>
     </div>
   );

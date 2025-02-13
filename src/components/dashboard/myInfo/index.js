2c73d3df85dd7807e3ad34f4fc63f7c4dcb105f5
byeongmin-kwak <bqudmals@naver.com>
2025-02-13 21:09:54 +0900

    feat: 대시보드 구성 요소 삭제 및 정리

--- src/components/dashboard/myInfo/index.js
+++
@@ -1,5 +0,0 @@
-import MyInfoTitle from "./ui/MyInfoTitle";
-import Name from "./ui/Name";
-import CoverLetter from "./ui/CoverLetter";
-
-export { MyInfoTitle, Name, CoverLetter };
--- src/layouts/dashboard/index.js
+++
@@ -1,4 +0,0 @@
-import MyInfo from "./ui/MyInfo";
-import ReviewHistory from "./ui/ReviewHistory";
-
-export { MyInfo, ReviewHistory };

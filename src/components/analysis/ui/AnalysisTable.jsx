27bca6e728662c163b49fe40affbcda3d2e66129
byeongmin-kwak <bqudmals@naver.com>
2025-02-21 14:48:49 +0900

    refactor: 불필요한 import 제거 및 코드 정리

--- src/components/analysis/ui/AnalysisTable.jsx
+++ src/components/analysis/ui/AnalysisTable.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styles from "./AnalysisTable.module.scss";
-import api from "api/axiosInstance";
 import { useNavigate } from "react-router-dom";
 import { useFetchAnalyses } from "api/analysisApi";
 

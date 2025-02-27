bba3de3341f016f15a62004ad0b56e7facadd1e7
byeongmin-kwak <bqudmals@naver.com>
2025-02-27 12:41:35 +0900

    feat: 로딩 처리 개선 및 옵셔널 체이닝 적용

--- src/privateRoute.js
+++ src/privateRoute.js
@@ -5,6 +5,7 @@ import { useAuth } from "auth/authContext";
 const PrivateRoute = ({ element }) => {
   const { isLoggedIn, loading, error } = useAuth();
 
+  // TODO: loading 처리 수정 필요
   if (loading) {
     return <div>Loading...</div>;
   }

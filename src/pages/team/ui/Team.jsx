d15450dcb4dbd7b9cd4078908df0956ff2dbf772
byeongmin-kwak <bqudmals@naver.com>
2025-03-13 14:45:42 +0900

    feat: 팀 페이지 및 스타일 컴포넌트 추가

---
+++ src/pages/team/ui/Team.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Team.module.scss";
+
+function Team() {
+  return (
+    <div className={styles.team}>
+      <div>서비스 구성원 소개</div>
+      <div>교수님~~</div>
+      <div>구성원~~</div>
+    </div>
+  );
+}
+
+export default Team;

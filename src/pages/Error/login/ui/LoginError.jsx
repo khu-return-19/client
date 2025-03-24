b2417baa898fdb2b1f0c29ce0d8ca827d9832ef5
byeongmin-kwak <bqudmals@naver.com>
2025-03-25 04:21:43 +0900

    feat: 로그인 오류 페이지 개선

--- src/pages/Error/login/ui/LoginError.jsx
+++ src/pages/Error/login/ui/LoginError.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./LoginError.module.scss";
+import { LoginModal } from "shared/loginModal";
 
 function LoginError() {
+  const [isLoginModalOpen, setLoginModalOpen] = useState(false);
+
   return (
     <div className={styles.loginError}>
-      <div className={styles.container}>
-        <span>로그인 필요</span>
-        <span>이 페이지에 접근하려면 로그인해야 합니다.</span>
+      <div className={styles.textArea}>
+        <span className={styles.title}>로그인이 필요한 서비스입니다.</span>
+        <span className={styles.info}>
+          로그인 후 장시간 자리를 비우셨거나 로그인하지 않으셨습니다. <br />
+          다시 로그인을 진행해주세요.
+        </span>
       </div>
+      <div className={styles.loginButton} onClick={() => setLoginModalOpen(true)}>
+        로그인
+      </div>
+      {isLoginModalOpen && <LoginModal onClose={() => setLoginModalOpen(false)} />}
     </div>
   );
 }

c77bf3a3652c49b1ddc7aacf4de58a16aa761a44
byeongmin-kwak <bqudmals@naver.com>
2025-03-25 16:51:23 +0900

    feat: PrivateRoute 컴포넌트에서 로딩 상태 처리 개선

--- src/privateRoute.js
+++ src/privateRoute.js
@@ -3,15 +3,10 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "auth/authContext";
 
 const PrivateRoute = ({ element }) => {
-  const { isLoggedIn, loading, error } = useAuth();
+  const { isLoggedIn, loading } = useAuth();
 
-  // TODO: loading 처리 수정 필요
   if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error occurred while checking authentication.</div>;
+    return null;
   }
 
   return isLoggedIn ? element : <Navigate to="/error" replace />;

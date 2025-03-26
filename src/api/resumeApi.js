7915484f1a1799199c1996b71b31c884fc84f42a
byeongmin-kwak <bqudmals@naver.com>
2025-03-26 21:27:25 +0900

    refactor: 불필요한 자기소개서 API 훅 제거 및 이력서 저장 훅 추가

--- src/api/resumeApi.js
+++ src/api/resumeApi.js
@@ -2,50 +2,6 @@ import api from "./axiosInstance";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { toast } from "react-toastify";
 
-// NOTE: 자기소개서 목록 조회
-export const useFetchResumes = () => {
-  return useQuery({
-    queryKey: ["resumes"],
-    queryFn: async () => {
-      const response = await api.get("/resumes");
-      return response.data;
-    },
-  });
-};
-
-// // NOTE: 자기소개서 조회
-// export const useFetchResume = (id) => {
-//   return useQuery({
-//     queryKey: ["resume", id],
-//     queryFn: async () => {
-//       const response = await api.get(`/resume/${id}`);
-//       return response.data;
-//     },
-//     enabled: !!id, // id가 있을 때만 쿼리 활성화
-//   });
-// };
-
-// // 자기소개서 수정
-// export const useUpdateResume = (id) => {
-//   return useMutation({
-//     mutationFn: (data) => api.patch(`/resume/${id}`, data),
-//   });
-// };
-
-// NOTE: 자기소개서 생성
-export const useCreateResume = () => {
-  return useMutation({
-    mutationFn: (data) => api.post("/resume", data),
-  });
-};
-
-// NOTE: 자기소개서 삭제
-export const useDeleteResume = () => {
-  return useMutation({
-    mutationFn: (resumeId) => api.delete(`/resume/${resumeId}`),
-  });
-};
-
 // NOTE: 이력서 조회
 export const useFetchResume = () => {
   return useQuery({
@@ -57,6 +13,7 @@ export const useFetchResume = () => {
   });
 };
 
+// NOTE: 이력서 저장
 export const useUpdateResume = () => {
   return useMutation({
     mutationFn: (data) => api.patch("/resume", data),

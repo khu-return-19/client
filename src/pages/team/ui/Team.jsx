f1cc5d582300b402840ddacf0bd7849d1db6976b
byeongmin-kwak <bqudmals@naver.com>
2025-04-15 14:42:00 +0900

    feat: 구성원 페이지에 구성원 이미지 추가 및 교수 소개 섹션 추가 및 반응형 디자인 구현

--- src/pages/team/ui/Team.jsx
+++ src/pages/team/ui/Team.jsx
@@ -16,11 +16,22 @@ function Team() {
       <div className={styles.wrapper}>
         <div className={styles.title}>서비스 제작 구성원 소개</div>
         <div className={styles.subtitle}>자기소개서 분석</div>
-        <img src="/team.png" alt="team" className={styles.teamImage} />
+        <div className={styles.professor}>
+          <img src="/team/team.png" alt="team" className={styles.teamImage} />
+          <span>
+            <span className={styles.title}>연혁</span> <br />
+            TTS커리어그룹 대표 및 경희대학교 미래인재센터 겸임교수
+            <br /> 2004년~현재 경희대학교 미래인재센터 취창업 컨설팅 담당교수 <br />
+            2018년~현재 경희대학교 메이커스페이스 Open Lab 담당교수 <br />
+            2023년~현재 경희대학교 KHU Careers Editor 지도교수
+            <br />
+            저서) 마이포트폴리오, 에잇블락협상모델
+          </span>
+        </div>
         <div className={styles.subtitle}>웹서비스 구축</div>
         <div className={styles.content}>
           <div className={styles.card}>
-            <div>이미지</div>
+            <img src="/team/byeongmin-kwak.jpg" alt="" />
             <div className={styles.description}>
               <span className={styles.name}>곽병민</span>
               <span className={styles.info}>
@@ -31,18 +42,18 @@ function Team() {
             </div>
           </div>
           <div className={styles.card}>
-            <div>이미지</div>
+            <img src="/team/wonjin-kim.jpeg" alt="" />
             <div className={styles.description}>
               <span className={styles.name}>김원진</span>
               <span className={styles.info}>
                 컴퓨터공학과 <br />
                 AI <br />
-                wjkim9653@khu.ac.kr
+                wjkim9653@gmail.com
               </span>
             </div>
           </div>
           <div className={styles.card}>
-            <div>이미지</div>
+            <img src="/team/seungpyo-hong.jpeg" alt="" />
             <div className={styles.description}>
               <span className={styles.name}>홍승표</span>
               <span className={styles.info}>
@@ -53,7 +64,7 @@ function Team() {
             </div>
           </div>
           <div className={styles.card}>
-            <div>이미지</div>
+            <img src="/team/hyorim-kim.jpeg" alt="" />
             <div className={styles.description}>
               <span className={styles.name}>김효림</span>
               <span className={styles.info}>

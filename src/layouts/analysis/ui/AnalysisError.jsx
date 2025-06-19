53b681bf659eb7da306fba066e3927b8aba48ed2
byeongmin-kwak <bqudmals@naver.com>
2025-06-20 01:12:33 +0900

    fix: 이미지 경로 수정 및 스타일 개선

--- src/layouts/analysis/ui/AnalysisError.jsx
+++ src/layouts/analysis/ui/AnalysisError.jsx
@@ -5,7 +5,7 @@ function AnalysisError() {
   return (
     <div className={styles.analysisError}>
       <div className={styles.wrapper}>
-        <img src="/shared/warning-icon" alt="" />
+        <img src="/shared/warning-icon.png" alt="" />
         <span className={styles.title}>자기소개서 분석에 실패했습니다.</span>
         <span className={styles.description}>
           분석 요청을 처리할 수 없습니다. <br />

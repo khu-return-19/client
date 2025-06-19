8bff67d10f19b868e0dbd817848f4686a1845117
byeongmin-kwak <bqudmals@naver.com>
2025-06-19 18:32:32 +0900

    fix: 남은 이용 횟수 표시 로직 수정

--- src/layouts/analyze/ui/AnalysisConfirmModal.jsx
+++ src/layouts/analyze/ui/AnalysisConfirmModal.jsx
@@ -8,7 +8,7 @@ function AnalysisConfirmModal({ isOpen, onClose, onConfirm, count }) {
       <div className={styles.content}>
         <div className={styles.title}>분석을 시작하겠습니까?</div>
         <div className={styles.info}>분석 횟수가 1회 차감됩니다.</div>
-        <div className={styles.count}>오늘 남은 이용 횟수 : {count} / 3</div>
+        <div className={styles.count}>오늘 남은 이용 횟수 : {3 - count} / 3</div>
       </div>
     </Modal>
   );

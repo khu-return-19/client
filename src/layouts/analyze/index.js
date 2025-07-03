0a88d3ab582829810600436542fc9c926a65227d
byeongmin-kwak <bqudmals@naver.com>
2025-07-03 22:11:18 +0900

    refactor: Info 레이아웃 분리

--- src/layouts/analyze/index.js
+++ src/layouts/analyze/index.js
@@ -2,5 +2,6 @@ import AnalysisConfirmModal from "./ui/AnalysisConfirmModal";
 import AnalyzeSkeleton from "./ui/AnalyzeSkeleton";
 import EmailSendModal from "./ui/EmailSendModal";
 import VerifyCodeModal from "./ui/VerifyCodeModal";
+import Info from "./ui/Info";
 
-export { AnalysisConfirmModal, AnalyzeSkeleton, EmailSendModal, VerifyCodeModal };
+export { AnalysisConfirmModal, AnalyzeSkeleton, EmailSendModal, VerifyCodeModal, Info };

bcdb18265e29735fbb782f2d408c12eff66508d7
byeongmin-kwak <bqudmals@naver.com>
2025-07-03 21:30:44 +0900

    refactor: Bottom 레이아웃 분리

--- src/pages/evaluation3D/ui/Evaluation3D.jsx
+++ src/pages/evaluation3D/ui/Evaluation3D.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import styles from "./Evaluation3D.module.scss";
-import GoToMainButton from "components/shared/goToMainButton";
-import { useNavigate } from "react-router-dom";
 import { Breadcrumb } from "components/shared/breadcrumb";
-import { TitleSection, EvaluationMethod, UtilizationValue } from "layouts/evaluation3D";
+import { TitleSection, EvaluationMethod, UtilizationValue, Bottom } from "layouts/evaluation3D";
 
 function Evaluation3D() {
-  const navigate = useNavigate();
-
   return (
     <div className={styles.evaluation3D}>
       <Breadcrumb paths={["서비스 소개", "3D 역량분석이란?"]} />
@@ -16,34 +12,7 @@ function Evaluation3D() {
         <EvaluationMethod />
         <UtilizationValue />
       </div>
-      <div className={styles.bottom}>
-        <div className={styles.content}>
-          <div className={styles.title}>서비스에 대해 더 구체적으로 알아보고 싶다면?</div>
-          <div className={styles.button}>
-            <div
-              className={styles.card}
-              onClick={() => {
-                navigate("/intro");
-                window.scrollTo(0, 0);
-              }}
-            >
-              <span className={styles.title}>서비스 소개</span>
-              <span className={styles.description}>자기소개서 역량평가 AI에 대한 전반적인 소개글과 활용 방안</span>
-            </div>
-            <div
-              className={styles.card}
-              onClick={() => {
-                navigate("/team");
-                window.scrollTo(0, 0);
-              }}
-            >
-              <span className={styles.title}>구성원</span>
-              <span className={styles.description}>pertineo 제작 구성원을 소개</span>
-            </div>
-          </div>
-        </div>
-        <GoToMainButton />
-      </div>
+      <Bottom />
     </div>
   );
 }

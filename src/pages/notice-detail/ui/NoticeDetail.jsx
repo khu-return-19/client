39f518cb95607355695b47573bb07da17679bcae
byeongmin-kwak <bqudmals@naver.com>
2025-07-03 18:12:11 +0900

    refactor: 공지사항 상세 페이지 - 불피요한 코드 제거

--- src/pages/notice-detail/ui/NoticeDetail.jsx
+++ src/pages/notice-detail/ui/NoticeDetail.jsx
@@ -3,7 +3,7 @@ import styles from "./NoticeDetail.module.scss";
 import { useParams, useNavigate } from "react-router-dom";
 import { useFetchNotice } from "api/noticeApi";
 import { AiOutlineMore } from "react-icons/ai";
-import { DeleteNoticeModal, NoticeDetailSkeleton } from "layouts/notice-detail";
+import { NoticeDetailSkeleton } from "layouts/notice-detail";
 
 function NoticeDetail() {
   const menuRef = useRef(null);
@@ -14,41 +14,6 @@ function NoticeDetail() {
   const navigate = useNavigate();
   const { id } = useParams();
   const { data: notice, isLoading } = useFetchNotice(id);
-
-  useEffect(() => {
-    function handleClickOutside(event) {
-      if (
-        menuRef.current &&
-        !menuRef.current.contains(event.target) && // 드롭다운 바깥 클릭
-        buttonRef.current &&
-        !buttonRef.current.contains(event.target) // 버튼 바깥 클릭
-      ) {
-        setMenuOpen(null);
-      }
-    }
-
-    if (menuOpen) {
-      document.addEventListener("mousedown", handleClickOutside);
-    }
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [menuOpen]);
-
-  const handleMenuToggle = (event, id) => {
-    event.stopPropagation();
-    setMenuOpen((prev) => (prev === id ? null : id));
-  };
-
-  const handleEdit = (id) => {
-    navigate(`/notice/${id}/edit`);
-  };
-
-  const handleDelete = (id) => {
-    setMenuOpen(null);
-    setIsModalOpen(true);
-  };
 
   const formatDate = (isoString) => {
     const date = new Date(isoString);
@@ -83,35 +48,14 @@ function NoticeDetail() {
             <div className={styles.title}>{notice.title}</div>
             <div className={styles.dateAndMenu}>
               <div className={styles.date}>{formatDate(notice.modifiedAt)}</div>
-              {/* {userInfo?.role === "admin" && (
-                <div className={styles.menuContainer}>
-                  <div
-                    className={styles.menuButton}
-                    onClick={(event) => handleMenuToggle(event, notice.id)}
-                    ref={buttonRef}
-                  >
-                    <AiOutlineMore />
-                  </div>
-                  {menuOpen === notice.id && (
-                    <div className={styles.menuDropdown} ref={menuRef}>
-                      <div onClick={(event) => handleEdit(notice.id)}>수정</div>
-                      <div onClick={(event) => handleDelete(notice.id)} className={styles.delete}>
-                        삭제
-                      </div>
-                    </div>
-                  )}
-                </div>
-              )} */}
             </div>
           </div>
-          {/* <div className={styles.body}>{notice.content}</div> */}
           <div dangerouslySetInnerHTML={{ __html: notice.content }} />
         </div>
       </div>
       <div className={styles.listButton} onClick={handleClick}>
         목록으로
       </div>
-      <DeleteNoticeModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} noticeId={id} />
     </div>
   );
 }

cd3477e693decdb10ec57f1c48b2fedf959f318e
byeongmin-kwak <bqudmals@naver.com>
2025-07-24 02:06:15 +0900

    style: 서비스 소개 - 레이아웃 수정 및 텍스트 수정

--- src/pages/intro/ui/Intro.jsx
+++ src/pages/intro/ui/Intro.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import styles from "./Intro.module.scss";
 import { Breadcrumb } from "components/shared/breadcrumb";
 import { TitleSection, KeyFeatures, ApplicationCases, Bottom } from "layouts/intro";
+import GoToMainButton from "components/shared/goToMainButton";
 
 function Intro() {
   return (
     <div className={styles.intro}>
-      <Breadcrumb paths={["서비스 소개"]} />
       <div className={styles.wrapper}>
+        <Breadcrumb paths={["서비스 소개"]} />
         <TitleSection />
         <KeyFeatures />
         <ApplicationCases />
       </div>
-      <Bottom />
+      <GoToMainButton />
     </div>
   );
 }

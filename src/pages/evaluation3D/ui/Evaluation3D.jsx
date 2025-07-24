55c9e78e91b808f14cf2ad64ea137871c9a74cd2
byeongmin-kwak <bqudmals@naver.com>
2025-07-24 20:24:48 +0900

    style: 3D 역량분석이란 - 레이아웃 수정

--- src/pages/evaluation3D/ui/Evaluation3D.jsx
+++ src/pages/evaluation3D/ui/Evaluation3D.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import styles from "./Evaluation3D.module.scss";
 import { Breadcrumb } from "components/shared/breadcrumb";
 import { TitleSection, EvaluationMethod, UtilizationValue, Bottom } from "layouts/evaluation3D";
+import GoToMainButton from "components/shared/goToMainButton";
 
 function Evaluation3D() {
   return (
     <div className={styles.evaluation3D}>
-      <Breadcrumb paths={["서비스 소개", "3D 역량분석이란?"]} />
       <div className={styles.wrapper}>
+        <Breadcrumb paths={["서비스 소개", "3D 역량분석이란?"]} />
         <TitleSection />
         <EvaluationMethod />
         <UtilizationValue />
       </div>
-      <Bottom />
+      <GoToMainButton />
     </div>
   );
 }

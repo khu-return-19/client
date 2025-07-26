61e210f33032cb1ec1c35a56375f78f8901dba47
byeongmin-kwak <bqudmals@naver.com>
2025-07-26 11:12:02 +0900

    style: 구성원 페이지 레이아웃 수정

--- src/pages/team/ui/Team.jsx
+++ src/pages/team/ui/Team.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import styles from "./Team.module.scss";
 import { Breadcrumb } from "components/shared/breadcrumb";
-import { Professor, Member, Bottom } from "layouts/team";
+import { Professor, Member } from "layouts/team";
+import GoToMainButton from "components/shared/goToMainButton";
 
 function Team() {
   return (
     <div className={styles.team}>
-      <Breadcrumb paths={["서비스 소개", "구성원"]} />
       <div className={styles.wrapper}>
+        <Breadcrumb paths={["서비스 소개", "구성원"]} />
         <div className={styles.title}>Pertineo 구성원</div>
         <Professor />
         <Member />
       </div>
-      <Bottom />
+      <GoToMainButton />
     </div>
   );
 }

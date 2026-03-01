855b89de1cb224de65ed3130215ad9a79c5ddded
geon-2 <gunle0416@gmail.com>
2026-03-01 15:12:54 +0900

    feat: 평가 방식 섹션 구현

--- src/pages/service-introduction/layouts/AnalysisSectionLayout.jsx
+++ src/pages/service-introduction/layouts/AnalysisSectionLayout.jsx
@@ -1,10 +1,12 @@
 import HeroSection from "../components/HeroSection"
 import AnalysisModel from "./analysis-section/AnalysisModel"
+import EvaluationMethod from "./analysis-section/EvaluationMethod"
 
 function AnalysisSectionLayout() {
     return (
         <HeroSection title="3D 역량분석이란?">
             <AnalysisModel />
+            <EvaluationMethod />
         </HeroSection>
     )
 }

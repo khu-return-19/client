2eadd0f52b7cc2c16cfe754453dc755f52c1c24b
jun-hwan00 <0528klkl@naver.com>
2026-03-09 20:42:31 +0900

    feat: 메인페이지 레이아웃 수정

--- src/pages/landing-page/1-main/layouts/MainSectionLayout.jsx
+++ src/pages/landing-page/1-main/layouts/MainSectionLayout.jsx
@@ -2,10 +2,10 @@ import HeroContent from "../components/HeroContent";
 
 function MainSectionLayout() {
   return (
-    <section
-      className="flex items-center justify-center w-full h-[736px]"
-    >
-      <HeroContent />
+    <section className="w-full h-[736px]">
+      <div className="max-w-[1440px] h-full mx-auto flex items-center justify-center">
+        <HeroContent />
+      </div>
     </section>
   );
 }
--- src/pages/landing-page/3-matrix/layouts/Landing3Layout.jsx
+++ src/pages/landing-page/3-matrix/layouts/Landing3Layout.jsx
@@ -1,7 +1,9 @@
 function Landing3Layout() {
   return (
-    <section className="flex items-center justify-center w-full h-[736px]">
-    
+    <section className="w-full h-[736px]">
+      <div className="max-w-[1440px] h-full mx-auto flex items-center justify-center">
+        {/* TODO: landing-3 (핵심지표) */}
+      </div>
     </section>
   );
 }

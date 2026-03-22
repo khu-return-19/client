91626f25e4b4e17ec327c8d0b0bbece1062adf47
jun-hwan00 <0528klkl@naver.com>
2026-03-22 20:09:33 +0900

    feat: 헤더 반응형 브레이크 포인트 추가

--- src/pages/input-page/layouts/InputPageLayout.jsx
+++ src/pages/input-page/layouts/InputPageLayout.jsx
@@ -4,7 +4,7 @@ import NavigationBar from "../components/NavigationBar";
 
 function InputPageLayout({ children, showRequired = true }) {
   return (
-    <div style={{ paddingTop: "var(--header-height)" }}>
+    <div className="pt-[clamp(52px,calc(2.5vw+28px),64px)]">
       <Header />
       <div className="min-[894px]:px-[40px] max-[893px]:px-[20px]">
         <TitleSection />

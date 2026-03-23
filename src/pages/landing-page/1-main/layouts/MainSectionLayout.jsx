806cf62725acc3073c41bd05f63950ba0f961a5d
jun-hwan00 <0528klkl@naver.com>
2026-03-23 18:44:06 +0900

    feat: main 섹션 레이아웃 수정

--- src/pages/landing-page/1-main/layouts/MainSectionLayout.jsx
+++ src/pages/landing-page/1-main/layouts/MainSectionLayout.jsx
@@ -3,15 +3,10 @@ import HeroContent from "../components/HeroContent";
 function MainSectionLayout() {
   return (
     <section className="w-full h-full relative bg-[#00010d] overflow-hidden">
-      {/* 좌하단 큰 */}
       <div className="absolute bottom-[-31.25vw] left-[-40vw] w-[61.5vw] h-[51.1vw] rounded-full bg-[#C1D9FF]" style={{ filter: "blur(13.9vw)" }} />
-      {/* 좌하단 작은 */}
       <div className="absolute bottom-[-5vw] left-[-28vw] w-[33.8vw] h-[31.1vw] rounded-full" style={{ background: "radial-gradient(circle, #002983, #266CDA)", filter: "blur(6.9vw)" }} />
-      {/* 우상단 큰 */}
       <div className="absolute top-[-20.25vw] right-[-20vw] w-[96.6vw] h-[20.97vw] rounded-full bg-[#0351CC]" style={{ filter: "blur(10.4vw)" }} />
-      {/* 우상단 작은 */}
       <div className="absolute top-[-25vw] right-[0vw] w-[46.6vw] h-[28.7vw] rounded-full" style={{ background: "radial-gradient(circle, #002983, #C1D9FF)", filter: "blur(5.6vw)" }} />
-      {/* 우하단 */}
       <div className="absolute bottom-[-8.75vw] right-[-20vw] w-[45.5vw] h-[15.8vw] rounded-full bg-[#428BFF]" style={{ filter: "blur(10.4vw)" }} />
 
       <div className="relative w-full h-full flex items-center justify-center">

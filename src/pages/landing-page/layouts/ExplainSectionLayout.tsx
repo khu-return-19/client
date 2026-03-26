c8efd36a65f43024a676d56633bd416f2776b198
mynameSumin <msm4167@naver.com>
2026-03-26 16:18:09 +0900

    feat: tailwind 반응형 breakPoint 설정

--- src/pages/landing-page/layouts/ExplainSectionLayout.tsx
+++ src/pages/landing-page/layouts/ExplainSectionLayout.tsx
@@ -5,7 +5,7 @@ export default function ExplainSectionLayout({
   children,
 }: ExplainSectionLayoutProps) {
   return (
-    <section className="flex flex-col px-[120px] pt-[100px] pb-[50px] bg-[#F4F6F8] w-screen h-screen">
+    <section className="flex flex-col px-[120px] min-[1441px]:px-[220px] pt-[100px] min-[1441px]:pt-[150px] pb-[50px] bg-[#F4F6F8] w-screen h-screen">
       {children}
     </section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "960px",
+      xl: "1440px"
+    }, 
     extend: {
       keyframes: {
         "fade-in": {

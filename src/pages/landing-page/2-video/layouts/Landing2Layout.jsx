fe6529b8ad8ba5a8eb1cf5e0975037153203d769
jun-hwan00 <0528klkl@naver.com>
2026-03-27 14:26:57 +0900

    feat: 비디오 섹션 패딩 수정

--- src/pages/landing-page/2-video/layouts/Landing2Layout.jsx
+++ src/pages/landing-page/2-video/layouts/Landing2Layout.jsx
@@ -1,17 +1,16 @@
-// import landingVideo from "assets/videos/video_test.mp4";
+import landingVideo from "assets/videos/video_test.mp4";
 
 function Landing2Layout() {
   return (
-    <section className="flex items-center justify-center w-screen h-full bg-black px-[clamp(0px,calc(12.5vw-120px),60px)]">
-      <div className="flex-1 h-full bg-[#333]" />
-      {/* <video
+    <section className="flex items-center justify-center w-screen h-full bg-black pt-[clamp(52px,calc(2.5vw+28px),64px)]">
+      <video
         src={landingVideo}
         autoPlay
         loop
         muted
         playsInline
-        className="w-full h-full object-cover"
-      /> */}
+        className="w-full lg:w-[clamp(calc(100vw-144px),calc(75vw+216px),100vw)] h-full object-cover"
+      />
     </section>
   );
 }

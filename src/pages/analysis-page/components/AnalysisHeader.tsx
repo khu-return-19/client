3dc5dcb0bbeb192599b9685f8c2af7f004ae3bd1
mynameSumin <msm4167@naver.com>
2026-04-13 20:37:46 +0900

    feat:전략 반영 api 연동

--- src/pages/analysis-page/components/AnalysisHeader.tsx
+++ src/pages/analysis-page/components/AnalysisHeader.tsx
@@ -1,7 +1,16 @@
+import cn from "utils/cn";
 interface AnalysisHeaderProps {
   title: string;
+  className?: string;
 }
 
-export default function AnalysisHeader({ title }: AnalysisHeaderProps) {
-  return <h2 className="text-navy900 font-semibold text-2xl mb-10">{title}</h2>;
+export default function AnalysisHeader({
+  title,
+  className,
+}: AnalysisHeaderProps) {
+  return (
+    <h2 className={cn(`text-navy900 font-semibold text-2xl mb-10`, className)}>
+      {title}
+    </h2>
+  );
 }

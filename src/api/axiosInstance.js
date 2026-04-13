b1d67e2b02ac1138c75856d66af499d4727d471e
mynameSumin <msm4167@naver.com>
2026-04-13 11:25:11 +0900

    fix: 자료형 수정

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -4,8 +4,8 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
   withCredentials: true,
   headers: {
-    'X-API-Version': 2
-  }
+    "X-API-Version": "2",
+  },
 });
 
 export default api;

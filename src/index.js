ca30f92f9ccb38608884a179be267f7daa2637a9
jun-hwan00 <0528klkl@naver.com>
2026-04-15 20:01:30 +0900

    feat: google analytics 키 수정

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "utils/reactQueryClient";
 
-ReactGA.initialize("G-1346J2JPLL");
+ReactGA.initialize(process.env.REACT_APP_GA_ID);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

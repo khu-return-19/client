708a39be7c79df7547a20a70bf81e43a92e514c8
jun-hwan00 <0528klkl@naver.com>
2026-04-17 00:14:12 +0900

    feat: main섹션 배경 교체

--- src/pages/landing-page/1-main/components/AnimatedBackground.jsx
+++ src/pages/landing-page/1-main/components/AnimatedBackground.jsx
@@ -1,29 +1,17 @@
 import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
-import bg from "assets/imgs/단계3.svg";
+import bg from "assets/imgs/landing.png";
 
 export function AnimatedBackground() {
-  const [stage, setStage] = useState(0);
-
-  useEffect(() => {
-    const timer1 = setTimeout(() => setStage(1), 1);
-    const timer2 = setTimeout(() => setStage(2), 1002);
-    return () => {
-      clearTimeout(timer1);
-      clearTimeout(timer2);
-    };
-  }, []);
-
   return (
     <div className="absolute inset-0 w-full h-full bg-[#000610] overflow-hidden">
       <motion.img
         src={bg}
         alt=""
-        className="absolute w-full left-0"
-        style={{ bottom: "-40%" }}
+        className="absolute left-0 w-full"
+        style={{ top: "calc(100vh - 43vw)" }}
         initial={{ opacity: 0 }}
-        animate={{ opacity: stage === 0 ? 0 : stage === 1 ? 0.5 : 1 }}
-        transition={{ duration: 1, ease: "easeInOut" }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 4, ease: "easeInOut" }}
       />
     </div>
   );

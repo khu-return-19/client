8c86605efdc8aee4bdc288d4e989d62756ff02b4
mynameSumin <msm4167@naver.com>
2026-04-20 13:35:55 +0900

    feat: 레포트 분석 횟수 api 연동

--- src/pages/input-page/components/AnalysisButton.jsx
+++ src/pages/input-page/components/AnalysisButton.jsx
@@ -1,19 +1,10 @@
 import Button from "./Button";
-import { useUserStore } from "stores/userStore";
-/**
- * 분석하기 전용 버튼 컴포넌트
- * @param {number} current - 분석 시도 횟수
- * @param {number} total - 최대 분석 횟수 (기본값 3)
- * @param {string} status - 'default' | 'disabled'
- */
-const AnalysisButton = ({
-  current = 0,
-  total = 3,
-  status = "default",
-  ...props
-}) => {
-  const count = useUserStore((state) => state.count);
+import { useNumOfAnalysis } from "api/analysisApi";
 
+const AnalysisButton = ({ total = 3, status = "default", ...props }) => {
+  const { data } = useNumOfAnalysis();
+  const count = data?.data.count ?? 0;
+  console.log(count);
   const countColor =
     status === "disabled" ? "text-[#EEEEEE]" : "text-[#C1D9FF]";
 

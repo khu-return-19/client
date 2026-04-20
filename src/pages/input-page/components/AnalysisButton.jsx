b555dc273379bb726c5d7e455122c7787455cc5a
mynameSumin <msm4167@naver.com>
2026-04-20 13:46:29 +0900

    feat: 횟수 전부 사용시 버튼 disabled 처리

--- src/pages/input-page/components/AnalysisButton.jsx
+++ src/pages/input-page/components/AnalysisButton.jsx
@@ -1,23 +1,28 @@
+import cn from "utils/cn";
 import Button from "./Button";
 import { useNumOfAnalysis } from "api/analysisApi";
 
 const AnalysisButton = ({ total = 3, status = "default", ...props }) => {
   const { data } = useNumOfAnalysis();
   const count = data?.data.count ?? 0;
-  console.log(count);
-  const countColor =
-    status === "disabled" ? "text-[#EEEEEE]" : "text-[#C1D9FF]";
 
   return (
     <Button
       size="M"
       variant="primary"
-      status={status}
+      status={count >= 3 ? "disabled" : status}
       className="gap-[10px]"
+      disabled={count >= 3 || status === "disabled"}
       {...props}
     >
       <span>분석하기</span>
-      <span className={`${countColor}`}>
+      <span
+        className={cn(
+          count >= 3 || status === "disabled"
+            ? "text-[#EEEEEE]"
+            : "text-[#C1D9FF]",
+        )}
+      >
         {3 - count}/{total}
       </span>
     </Button>
